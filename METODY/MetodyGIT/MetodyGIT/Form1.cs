e2f8b26bcbc8ede5e9d24ea2b21ba75b87faf084
Prášek <Prek@DESKTOP-89RHD4J>
2021-03-08 13:05:03 +0100

    Metody06.1

--- METODY/MetodyGIT/MetodyGIT/Form1.cs
+++ METODY/MetodyGIT/MetodyGIT/Form1.cs
@@ -16,10 +16,23 @@ namespace MetodyGIT
         {
             InitializeComponent();
         }
+        int CifLichSoucet(int a)
+        {
+            int soucet = 0;
+            while (a > 0)
+            {
+                if ((a % 10) % 2 != 0) soucet += a % 10;
+                a /= 10;
+            }
+            return soucet;
+        }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            int a = Convert.ToInt32(textBox1.Text);
+            label1.Text = CifLichSoucet(a).ToString();
         }
     }
 }
